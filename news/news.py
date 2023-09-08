a8d2787024c5413c0e2bf095b9e92298bd408efa
lebelokamogelo <lebelokamogelo47@gmail.com>
2023-09-08 02:52:01 +0200

    Integrate BeautifulSoup for web scraping news data

---
+++ news/news.py
@@ -0,0 +1,42 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+
+
+# Function to get news or article from the internet
+def get_random_news():
+    # URL of the website you want to scrape
+    url = 'https://www.limpopomirror.co.za/articles/venda'
+
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Check (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the elements
+        articles = soup.find_all('div', class_='art')
+
+        # Random article
+        index = random.randint(0, len(articles) - 1)
+
+        # noinspection PyBroadException
+        try:
+            title = articles[index].find('h3').text.strip()
+            date = articles[index].find('span', class_="date").text.strip()
+            author = "Unknown"
+            content = articles[index].find('div', class_="syn").find('p').text.strip()
+
+            if title is None:
+                return get_random_news()
+            else:
+                return f"""
+                        Date: {date}
+                        Author: {author}
+                        Title: {title}
+                        Content: {content}
+                        """
+        except:
+            return get_random_news()
